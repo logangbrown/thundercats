a08a399cdf8fb4d03fcdbb5438f0163c1d48b796
Ryden7 <Rizwan.M.Mohammed1@gmail.com>
2018-03-19 21:12:57 -0600

    Initial commit

---
+++ time_sucks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace time_sucks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
