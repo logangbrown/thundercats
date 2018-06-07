f0ecc69625fb6cf58322f92e42e931e8e75547da
Natasha <natasha.aguilar@comcast.net>
2018-06-07 17:45:01 -0600

    Fixed hash function - now can successfully log in

--- time_sucks/Controllers/HomeController.cs
+++ time_sucks/Controllers/HomeController.cs
@@ -125,18 +125,9 @@ namespace time_sucks.Controllers
             SHA256 sha256 = SHA256Managed.Create();
             byte[] bytes = Encoding.UTF8.GetBytes(password);
             byte[] hash = sha256.ComputeHash(bytes);
-            return GetStringFromHash(hash);
+            return Convert.ToBase64String(hash);
         }
         
-        public static string GetStringFromHash(byte [] hash)
-        {
-            StringBuilder result = new StringBuilder();
-            for(int i = 0; i < hash.Length; i++)
-            {
-                result.Append(hash[i].ToString("X2"));
-            }
-            return result.ToString();
-        }
 
         [HttpGet]
         public IActionResult GetUsers()
@@ -174,6 +165,7 @@ namespace time_sucks.Controllers
             User DBUser = null;
             if(DBHelper.getUser(user.username) != null)
             {
+                user.password = GenerateHash(user.password);
                 DBUser = DBHelper.getUser(user.username, user.password);
             } else
             {
